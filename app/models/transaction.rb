1aea59ac8f40dbec13417332be8e29a734f68f08
Alexander Dreher <alexdreher@lxmedia.net>
2010-11-30 16:24:31 +0100

    refactored booking (transaction/new) javascripts, mixed with new validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,17 +4,25 @@ class Transaction < ActiveRecord::Base
   
   before_validation :create_booking_date
   
-  attr_accessor :book_date, :debit, :credit
+  attr_accessor :book_date
   
   # book_date format: [01-31][01-12][dd], where d = any digit
   validates_format_of :book_date, :with => /^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])(\d{2})$/
   
   validates_presence_of :booking_date
   
-  validates_presence_of :credit_id
-  validates_presence_of :debit_id
+  # a lot of validations for one thing...
+  #validates_presence_of :credit_id
+  #validates_presence_of :debit_id
+  
+  #validates_associated :credit_account
+  #validates_associated :debit_account
+  
+  validates_inclusion_of :debit, :in => Account.all.map { |a| a.number }
+  validates_inclusion_of :credit, :in => Account.all.map { |a| a.number }
   
   validates_format_of :amount, :with => /^\d{1,10}(\.\d{1,2})?$/
+  
   
   # creates the booking date or leaves it blank, if the user input is crap
   def create_booking_date
@@ -23,12 +31,22 @@ class Transaction < ActiveRecord::Base
   rescue
   end
   
+  def debit
+    self.debit_account.number
+  rescue
+  end
   
   def debit=(num)
     self.debit_account = Account.find_by_number(num)
   end
   
+  def credit
+    self.credit_account.number
+  rescue
+  end
+  
   def credit=(num)
     self.credit_account = Account.find_by_number(num)
   end
+
 end
--- public/javascripts/transactions.js
+++ public/javascripts/transactions.js
@@ -1,16 +1,46 @@
-$('#transaction_book_date').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_debit').select();return false; }});
-$('#transaction_debit').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_credit').select();return false; }});
-$('#transaction_credit').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_amount').select();return false; }});
-$('#transaction_amount').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_title').select();return false; }});
-$('#transaction_title').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_submit').focus();return false; }});
+var accounting = {
+  setupAjaxCallbacks: function(){
+    $('body').ajaxStart(function () {
+      $('#ajax_status').show().text("Loading...");
+    });
+    
+    $('body').ajaxStop(function () {
+      $('#ajax_status').fadeOut();
+    });
+  },
+  
+  ajaxifyForm: function() {
+    $form = $("#new_transaction");
+    
+    $form.submit(function(e) {
+      e.preventDefault();
+      $.post($form.attr('action')+ '.js', $form.serializeArray(), null, 'script');
+      $('#transaction_book_date').select();
+    });    
+  },
+  
+  /** 
+  * this method overwrites the enter key in a form field 
+  * standard behavior: submit the form
+  * desired behavior: jump to the next input field
+  *   like pressing the TAB-key
+  */
+  enterizeFormFields: function(){
+    $('#transaction_book_date').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_debit').select();return false; }});
+    $('#transaction_debit').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_credit').select();return false; }});
+    $('#transaction_credit').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_amount').select();return false; }});
+    $('#transaction_amount').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_title').select();return false; }});
+    $('#transaction_title').keypress(function(event){ if(event.keyCode == 13) { $('#transaction_submit').focus();return false; }});
+  },
+};
 
-$("#new_transaction").submit(function() {
-  $.post($(this).attr('action')+ '.js', 
-  $(this).serializeArray(), null, 'script');
+jQuery(function () {
+  accounting.setupAjaxCallbacks();
+  
+  accounting.ajaxifyForm();
+  
+  accounting.enterizeFormFields();
+  
+  // select the first input field to be abled to start typing right away
   $('#transaction_book_date').select();
-  return false;
 });
-
-$(document).ready(function() {
-	$('#transaction_book_date').select();
-} );
