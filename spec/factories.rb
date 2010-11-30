b6859eef4431a7eab5e777b82504c12d1ae472d5
Alexander Dreher <alexdreher@lxmedia.net>
2010-11-30 14:15:56 +0100

    prepared factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,5 @@
 Factory.define :account do |f|
+  # f.sequence(:number) { |n| 1501+n }
   f.number 1501
   f.title "Title"
   f.account_type_id 1
@@ -7,6 +8,8 @@ end
 Factory.define :transaction do |f|
   f.book_date "241013"
   f.debit 1501
+  #f.association :debit, :factory => :account
+  #f.association :credit, :factory => :account
   f.credit 1502
   f.amount 1.5
   f.title "Title"
